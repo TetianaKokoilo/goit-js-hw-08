8d42ff5934f7bd2d88a029e44b0464efa1d6e0e4
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-01-08 19:09:19 +0200

    add task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,49 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const imageGallery = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    item =>
+      `<div class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+                <img class="gallery__image"
+                src ="${item.preview}" 
+                data-source = "${item.original}" 
+                alt="${item.description}"/>
+            </a>
+        </div>`
+  )
+  .join('');
+
+imageGallery.insertAdjacentHTML('beforeend', markup);
+
+console.log(imageGallery);
+
+imageGallery.addEventListener('click', onClick);
+
+function onClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+    openModal(event.target.dataset.source);
+
+};
+
+function openModal(evt) {
+  let gallery = new SimpleLightbox('.gallery a');
+  gallery.on('show.simplelightbox', () => {
+    `<div class="modal">
+        <img src="${evt}">
+    </div>`      
+  });
+};
+
+
