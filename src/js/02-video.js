c0154dc0f3e1de7e553e7254b5f0ee460f8c3484
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-01-08 19:58:01 +0200

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,36 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+setTime();
+
+player.on('timeupdate', throttle(getNewTime, 1000));
+
+function getNewTime() {
+  player
+    .getCurrentTime()
+    .then(function (seconds) {
+      localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+function setTime() {
+  const startTime = localStorage.getItem(LOCALSTORAGE_KEY);
+  player.setCurrentTime(startTime).catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+}
